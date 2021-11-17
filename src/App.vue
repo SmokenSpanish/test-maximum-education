499fdfd859ac50a89a0099ab389e4848996e9554
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-17 17:38:27 +0300

    feat: new app start

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <FormApp />
+</template>
+
+<script>
+import FormApp from './components/FormApp.vue'
+
+export default {
+  name: 'App',
+  components: { FormApp }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background-color: #eee;
+}
+</style>
